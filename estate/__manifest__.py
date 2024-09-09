757dddbb223cc42a2e67d106b809d297d1dfeb31
Manuel Uzueta <muzueta@pao-mx.com>
2024-09-09 12:07:37 -0600

    Se agregan menus

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -8,6 +8,7 @@
         'security/ir.model.access.csv',
         
         'views/estate_property_views.xml'
+        'views/estate_menus.xml'
     ],
     "installable": True,
     'license': 'LGPL-3',
