1454b3a44100e04b99f895e20a23a839f660ad43
Manuel Uzueta <muzueta@pao-mx.com>
2024-09-09 15:21:45 -0600

    Se agregan campos salesman y buyer

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -29,7 +29,7 @@ class EstateProperty(models.Model):
     
     type = fields.Many2one("estate.property.type", string="Type")
     
-    salesman = fields.Many2one('res.user', string='Salesman')
+    salesman = fields.Many2one('res.users', string='Salesman')
     buyer = fields.Many2one('res.partner', string='Buyer')
     
     class EstatePropertyType(models.Model):
