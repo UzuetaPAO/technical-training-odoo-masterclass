823591c43b6b1e05c6f2108a2d3dd1f9fcea60b6
Manuel Uzueta <muzueta@pao-mx.com>
2024-09-09 16:26:28 -0600

    Se agregan los campos validity y date_deadline

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -69,15 +69,15 @@ class EstateProperty(models.Model):
         partner_id = fields.Many2one('res.partner', string="Potential Buyer", required=True)
         property_id = fields.Many2one('estate.property', string="Estate Property", required=True)
         
-        validity = fields.Integer('Validity', default=7)
-        date_deadline = fields.Date('Date Deadline', compute="_compute_date_deadline", inverse="_compute_validity_days")
+        validity = fields.Integer('Validity', default=7, compute="_compute_validity_days", inverse="_compute_date_deadline")
+        date_deadline = fields.Date('Date Deadline')
         
-        @api.depends("validity")
         def _compute_date_deadline(self):
             for record in self:
                 create_date = record.create_date if record.id else date.today()
                 record.date_deadline = create_date + relativedelta(days=record.validity)
                 
+        @api.depends("date_deadline")
         def _compute_validity_days(self):
             for record in self:
                 create_date = record.create_date if record.id else date.today()
