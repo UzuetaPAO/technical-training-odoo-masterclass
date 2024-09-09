883b6a8a2458fa007314c03e7006dc08f3b19751
Manuel Uzueta <muzueta@pao-mx.com>
2024-09-09 13:59:18 -0600

    Se actualizan dependencias

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -2,7 +2,7 @@
     "name": "Estate",  # The name that will appear in the App list
     "version": "16.0.0",  # Version
     "application": True,  # This line says the module is an App, and not a module
-    "depends": ["base"],  # dependencies 
+    "depends": ["base", "contacts", "hr"],  # dependencies 
     'author': 'Manuel Uzueta Gil',
     "data": [
         'security/ir.model.access.csv',
