eb1a39d66ffe58d2473f296fe1b00a27bbb7d087
Manuel Uzueta <muzueta@pao-mx.com>
2024-09-09 11:27:06 -0600

    Se agregan caracterísitcas a modulo Estate

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -3,9 +3,9 @@
     "version": "16.0.0",  # Version
     "application": True,  # This line says the module is an App, and not a module
     "depends": ["base"],  # dependencies
-    'author': 'Manuel Uzueta changed by Editor 2',
+    'author': 'Manuel Uzueta Gil',
     "data": [
-
+        'security/ir.model.access.csv',
     ],
     "installable": True,
     'license': 'LGPL-3',
---
+++ estate/models/__init__.py
@@ -0,0 +1,1 @@
+import estate_property
