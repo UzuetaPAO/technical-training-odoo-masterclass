78ac8196a63e02eeba646fe5dc18a248fa28ed83
Manuel Uzueta <muzueta@pao-mx.com>
2024-09-10 17:25:14 -0600

    Se agrega conexion entre modulos

--- estate_account/models/estate_property_inherit.py
+++ estate_account/models/estate_property_inherit.py
@@ -7,4 +7,23 @@ class EstatePropertyInherit(models.Model):
 
     def action_sold_property(self):
         _logger.warning("INHERIT WORKS")
+        invoice = self.env['account.move'].create({
+            'name': 'INV - ' + self.name,
+            'move_type': 'out_invoice',
+            'partner_id': self.buyer.id
+        })
+        
+        invoice_line_6_percent = self.env['account.move.line'].create({
+            'name': self.name,
+            'quantity': 1,
+            'price_unit': self.selling_price * 0.06,
+            'move_id': invoice.id,
+        })
+        
+        invoice_line_fees = self.env['account.move.line'].create({
+            'name': 'Fees',
+            'quantity': 1,
+            'price_unit': 100,
+            'move_id': invoice.id,
+        })
         return super().action_sold_property()
