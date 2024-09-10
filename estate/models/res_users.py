7e3a87058503b4a35f55e875f0ae63ac89fc800d
Manuel Uzueta <muzueta@pao-mx.com>
2024-09-10 16:28:20 -0600

    Se agregan caracterísitcas de herencia

--- estate/models/res_users.py
+++ estate/models/res_users.py
@@ -3,4 +3,4 @@ from odoo import fields, models
 class InheritedModel(models.Model):
     _inherit = "res.users"
 
-    property_ids = fields.One2many('estate.property', 'salesman', string="Properties", domain="[('id', '=', salesman)]")
+    property_ids = fields.One2many('estate.property', 'salesman', string="Properties", domain="[('salesman', '=', id)]")
