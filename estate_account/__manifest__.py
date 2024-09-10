0a0ffb350e9d8c76851e39757d1b084495d40a99
Manuel Uzueta <muzueta@pao-mx.com>
2024-09-10 16:48:18 -0600

    Se agregan caracterísitcas de herencia

---
+++ estate_account/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Estate Account",  # The name that will appear in the App list
+    "version": "16.0.0",  # Version
+    "application": False,  # This line says the module is an App, and not a module
+    "depends": ["account", "estate"],  # dependencies 
+    'author': 'Manuel Uzueta Gil',
+    "data": [
+        
+    ],
+    "installable": True,
+    'license': 'LGPL-3',
+}
